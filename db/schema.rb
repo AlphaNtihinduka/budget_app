ef42c25a791bb5ad1af49ed6202262ff3c23e93c
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-23 13:52:36 +0300

    Create categories table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_22_214632) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_23_072641) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -21,6 +21,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_214632) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "category_payments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "category_id", null: false
+    t.bigint "payment_id", null: false
+    t.index ["category_id"], name: "index_category_payments_on_category_id"
+    t.index ["payment_id"], name: "index_category_payments_on_payment_id"
   end
 
   create_table "payments", force: :cascade do |t|
@@ -46,5 +55,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_214632) do
   end
 
   add_foreign_key "categories", "users"
+  add_foreign_key "category_payments", "categories"
+  add_foreign_key "category_payments", "payments"
   add_foreign_key "payments", "users"
 end
