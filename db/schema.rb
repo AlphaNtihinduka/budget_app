d0b165a499bcade64d8092e577af378a8f95ddca
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-24 20:55:08 +0300

    Create user spec environment

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_23_225142) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_24_174510) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_23_225142) do
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_payments_on_user_id"
+  end
+
+  create_table "splashes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
