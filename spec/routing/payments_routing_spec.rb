dd980bc433219bc92a5c5a374f9360c746806ceb
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-25 00:16:15 +0300

    Testing the payments routing

--- spec/routing/payments_routing_spec.rb
+++ spec/routing/payments_routing_spec.rb
@@ -2,37 +2,29 @@ require "rails_helper"
 
 RSpec.describe PaymentsController, type: :routing do
   describe "routing" do
-    it "routes to #index" do
-      expect(get: "/payments").to route_to("payments#index")
-    end
-
     it "routes to #new" do
-      expect(get: "/payments/new").to route_to("payments#new")
-    end
-
-    it "routes to #show" do
-      expect(get: "/payments/1").to route_to("payments#show", id: "1")
+      expect(get: "/categories/1/payments/new").to route_to("payments#new", category_id: "1")
     end
 
     it "routes to #edit" do
-      expect(get: "/payments/1/edit").to route_to("payments#edit", id: "1")
+      expect(get: "/categories/1/payments/1/edit").to route_to("payments#edit", category_id: "1", id: "1")
     end
 
 
     it "routes to #create" do
-      expect(post: "/payments").to route_to("payments#create")
+      expect(post: "categories/1/payments").to route_to("payments#create", category_id: "1")
     end
 
     it "routes to #update via PUT" do
-      expect(put: "/payments/1").to route_to("payments#update", id: "1")
+      expect(put: "categories/1/payments/1").to route_to("payments#update", category_id: "1", id: "1")
     end
 
     it "routes to #update via PATCH" do
-      expect(patch: "/payments/1").to route_to("payments#update", id: "1")
+      expect(patch: "categories/1/payments/1").to route_to("payments#update", category_id: "1", id: "1")
     end
 
     it "routes to #destroy" do
-      expect(delete: "/payments/1").to route_to("payments#destroy", id: "1")
+      expect(delete: "categories/1/payments/1").to route_to("payments#destroy", category_id: "1", id: "1")
     end
   end
 end
