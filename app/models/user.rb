f8b134ee6b2411ba558a61da6d16aec6dad3f994
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-24 23:12:54 +0300

    Validate user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   has_many :categories, dependent: :destroy
   has_many :payments, dependent: :destroy
+  validates :name, presence: true, length: { maximum: 40 }
 end
