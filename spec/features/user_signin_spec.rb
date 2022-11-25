23082485eab63dc2de991f65a06d9b03caee8154
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-25 11:53:14 +0300

    Testing user sign in

---
+++ spec/features/user_signin_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.feature 'UserSignins', type: :feature do
+  before :each do
+    @user = User.new(name: 'NTIHINDUKA ALPHA', email: 'ntihindukaalpha@yahoo.com', password: '1234567890',
+                     password_confirmation: '1234567890')
+    @user.save
+  end
+
+  it 'signs me in' do
+    visit '/users/sign_in'
+    within('#new_user') do
+      fill_in 'user_email', with: 'ntihindukaalpha@yahoo.com'
+      fill_in 'user_password', with: '1234567890'
+    end
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully'
+  end
+end
