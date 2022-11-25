5a555601621008aabd063870cca0cc6e84669dea
AlphaNtihinduka <ntihindukaalpha@yahoo.com>
2022-11-25 13:13:48 +0300

    Confirm user spec

--- spec/features/user_signin_spec.rb
+++ spec/features/user_signin_spec.rb
@@ -5,6 +5,7 @@ RSpec.feature 'UserSignins', type: :feature do
     @user = User.new(name: 'NTIHINDUKA ALPHA', email: 'ntihindukaalpha@yahoo.com', password: '1234567890',
                      password_confirmation: '1234567890')
     @user.save
+    @user.confirm
   end
 
   it 'signs me in' do
@@ -15,5 +16,25 @@ RSpec.feature 'UserSignins', type: :feature do
     end
     click_button 'Log in'
     expect(page).to have_content 'Signed in successfully'
+  end
+
+  it 'signs me in' do
+    visit '/users/sign_in'
+    within('#new_user') do
+      fill_in 'user_email', with: 'ntihindukaalpha@yahoo.com'
+      fill_in 'user_password', with: '1234567890'
+    end
+    click_button 'Log in'
+    expect(page).to have_content 'Categories'
+  end
+
+  it 'signs me in' do
+    visit '/users/sign_in'
+    within('#new_user') do
+      fill_in 'user_email', with: 'ntihindukaalpha@yahoo.com'
+      fill_in 'user_password', with: '1234567890'
+    end
+    click_button 'Log in'
+    expect(page).to have_content(@user.name)
   end
 end
